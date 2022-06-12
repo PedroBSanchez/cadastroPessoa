edab99691aae1eaadb7b98e004cead9e761e6007
PedroBSanchez <pedro_b47@hotmail.com>
2022-06-12 01:26:49 -0300

    Alterações back-end

--- back/modules/pessoaController.js
+++ back/modules/pessoaController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const validaCpf = require("../src/utils/validators/cpfValidator");
+const findByCpf = require("../src/utils/queries/findByCpf");
 
 const PessoaModel = require("../src/models/pessoa.model");
 
@@ -25,25 +26,15 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// Implementar findByCpf
-router.get("/teste/:cpf", async (req, res) => {
-  let cpfBanco = req.params.cpf;
-  try {
-    const pessoa = await PessoaModel.find({
-      cpf: cpfBanco,
-    });
-    res.status(200).json(pessoa);
-  } catch (error) {
-    res.status(500).send("Pessoa não encontrada");
-  }
-});
-
 router.post("/", async (req, res) => {
+  let cpfValido = validaCpf(req.body.cpf);
+  let cpfExistente = findByCpf(req.body.cpf);
   try {
-    if (validaCpf(req.body.cpf)) {
+    if (cpfValido && !cpfExistente) {
       const pessoa = await PessoaModel.create(req.body);
       res.status(200).send("Pessoa cadastrada com sucesso");
-    } else res.status(400).send("CPF inválido");
+    } else if (!cpfValido) res.status(400).send("CPF inválido");
+    else res.status(400).send("CPF já cadastrado");
   } catch (error) {
     res.status(500).send(error.message);
   }
@@ -59,7 +50,9 @@ router.put("/:id", async (req, res) => {
       }
     );
     res.status(200).send("Pessoa atualizada com sucesso");
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
